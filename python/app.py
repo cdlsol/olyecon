c64f570fb9db9ca25572c4fd6d118fded4660c6c
cdlsol <carloslopez.cl265@gmail.com>
2024-10-25 20:27:45 -0600

    Added dashboard cards ffor raw data frame and another one for medals per country. Also added a dynamic input to adjust the number of items in bar plot.

--- python/app.py
+++ python/app.py
@@ -9,21 +9,29 @@ from pipe import ELTPipeline
 
 ui.page_opts(title = "Countries Olympic Medals vs their Population and GDP", fillable=True)
 
+with ui.sidebar():
+    "Inputs (add)"
+"Main Content"
+
+
 @reactive.calc
 def getdata():
     pipeline = ELTPipeline()
     df = pipeline.extract()
     return df
-    
 
-with ui.layout_columns():
-    
-    @render_plotly
-    def total_medals_plot():
-        df = getdata()
-        top_5 = df.groupby('country_code')['total'].sum().nlargest(5).reset_index()
-        return px.bar(top_5, x = 'country_code', y = 'total')
+with ui.card():
 
+    ui.input_numeric("n", "Number of items in bar plot", 5, min = 1, max = 88)
+
+    with ui.layout_columns():
+        @render_plotly
+        def total_medals_plot():
+            df = getdata()
+            top_n = df.groupby('country_code')['total'].sum().nlargest(input.n()).reset_index()
+            return px.bar(top_n, x = 'country_code', y = 'total')
+
+with ui.card():
 
     @render.data_frame
     def showtable():
