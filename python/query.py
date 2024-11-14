78cabb0ccc4f525a9859ac46de2110c45d4b34b8
cdlsol <carloslopez.cl265@gmail.com>
2024-11-13 22:06:08 -0600

    Fixed df fectch with duckdb from postgresql db

--- python/query.py
+++ python/query.py
@@ -2,52 +2,49 @@ import pandas as pd
 import duckdb 
 from sqlalchemy import create_engine
 
+class Querydb:
 
-def query_postgre_duck():
-    #Connection parameters
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'pgdatabase'
-    pg_port = '5432'
-    pg_db = 'olympics24db'
+    @staticmethod
 
-    try:
-        #Postgresql connection string
-        pg_connection_string = f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}"
+    def query_postgre_duck():
+        #Connection parameters
+        pg_user = 'root'
+        pg_password = 'root'
+        pg_host = 'pgdatabase'
+        pg_port = '5432'
+        pg_db = 'olympics24db'
 
-        #Cretae Duckdb connection
-        duck_conn = duckdb.connect()
+        try:
+            #Postgresql connection string
+            pg_connection_string = f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}"
 
-        #Install and load Postgresql extension for duckdb
-        duck_conn.install_extension("postgres_scanner")
-        duck_conn.load_extension("postgres_scanner")
+            #Cretae Duckdb connection
+            duck_conn = duckdb.connect()
 
-        #Attach to Postgresql db
-        duck_conn.execute(f"""
-        ATTACH '{pg_connection_string}' AS olympics24db (TYPE postgres);
-        """)
+            #Install and load Postgresql extension for duckdb
+            duck_conn.install_extension("postgres_scanner")
+            duck_conn.load_extension("postgres_scanner")
 
-        #list all tables (test query)
-        tables = duck_conn.execute("""
-        SELECT * FROM olympics24db.information_schema.tables
-        WHERE table_schema = 'public';        
-        """).fetchdf()
+            #Attach to Postgresql db
+            duck_conn.execute(f"""
+            ATTACH '{pg_connection_string}' AS olympics24db (TYPE postgres);
+            """)
 
-        print("\nAvailable tables:")
-        print(tables)
-
-        # target data (test query)
-        data = duck_conn.execute("""
-        SELECT * FROM olympics24db.public.olyecon LIMIT 5;
-        """).fetchdf()
-        print("\nSample data from olyecon table:")
-        print(data)
-    
-    except Exception as e:
-        print(f"An error occurred:{e}")
-    finally:
-        duck_conn.close()
+            # target data (test query)
+            df = duck_conn.execute("""
+            SELECT * FROM olympics24db.public.olyecon;
+            """).fetch_df()
+            print("\nAll data from olyecon table:")
+            print(df)
+            return df
+        
+        except Exception as e:
+            print(f"An error occurred:{e}")
+        finally:
+            duck_conn.close()
 
 if __name__ == "__main__":
-    query_postgre_duck()
 
+    # querydata = Querydb()
+    # df = querydata.query_postgre_duck()
+    df = Querydb.query_postgre_duck() 
